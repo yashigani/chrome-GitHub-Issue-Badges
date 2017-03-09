9f5244d2c5f23fa9a2bd3b633bf04fcff50a6255
yashigani <yashigani_w@hatena.ne.jp>
2017-03-09 19:12:34 +0900

    複数のGHE環境を登録できるように変更した

--- src/ts/options.ts
+++ src/ts/options.ts
@@ -1,58 +1,67 @@
-function getConfig(elm: any) {
-    const origin  = elm.querySelector('[name="origin"]').value;
-    const apiRoot = elm.querySelector('[name="api-root"]').value;
-    const token   = elm.querySelector('[name="token"]').value;
-
-    return origin && apiRoot && token ? {
-        origin  : origin,
-        apiRoot : apiRoot,
-        token   : token
-    } : undefined;
+interface Config {
+    origin?: string,
+    apiRoot?: string,
+    token?: string,
 }
 
-function fillInConfig(elm: any, config: any) {
-    if (!(config.origin && config.apiRoot && config.token)) { return }
-    elm.querySelector('[name="origin"]').value = config.origin
-    elm.querySelector('[name="api-root"]').value = config.apiRoot
-    elm.querySelector('[name="token"]').value = config.token
+function getTextArea(): HTMLTextAreaElement {
+    let elm = document.querySelector('.configuration') as HTMLTextAreaElement;
+    return elm;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    const configs: any[] = JSON.parse(localStorage.getItem('origins') || '[]');
-    configs.forEach(config => {
-        const elm: any = document.querySelector('.github-config')
-        fillInConfig(elm, config)
-    })
+function getConfigurations(elm: HTMLTextAreaElement) {
+    let text = elm.value;
+    let configurations = JSON.parse(text);
+    return configurations;
+}
 
-    document.querySelector('.github-config [name="origin"]').addEventListener('input', e => {
-        var elm: any = document.querySelector('.github-config [name="api-root"]');
-        elm.value = (<HTMLInputElement>e.target).value + '/api/v3';
-    })
+function fillInConfig(elm: HTMLTextAreaElement, configs: any[]) {
+    let json = JSON.stringify(configs, null, '  ');
+    elm.value = json;
+}
 
-    document.querySelector('.save-button').addEventListener('click', function(e) {
-        const github = getConfig(document.querySelector('.github-config'));
-        const configs = [github].filter(c => c !== undefined)
-        localStorage.setItem('origins', JSON.stringify(configs))
-        alert('saved')
-    })
-
-    Array.prototype.forEach.call(document.querySelectorAll('.test-button'), (button: HTMLButtonElement) => {
-        button.addEventListener('click', function(e: any) {
-            const config = getConfig(e.target.parentNode);
-            if (config === undefined) {
-                alert('please input')
-                return;
-            }
+function testToken(config: Config): Promise<string> {
+    if (config === undefined) {
+        return;
+    } else {
+        return new Promise((resolve, reject) => {
             const xhr = new XMLHttpRequest();
             xhr.open("GET", config.apiRoot);
             xhr.setRequestHeader("Authorization", `token ${config.token}`)
-            xhr.onload  = function(e) {
-                alert('connection ' + xhr.statusText + ': ' + xhr.responseText)
-            }
-            xhr.onerror = function(e) {
-                alert('connection failed')
-            }
+            xhr.onload = (r) => {
+                if (xhr.status === 200) {
+                    resolve(config.origin);
+                } else {
+                    reject(config.origin);
+                }
+            };
+            xhr.onerror = (e) => {
+                reject(config.origin);
+            };
             xhr.send();
-        })
-    })
-})
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    let defaultConfigurations = '[{"origin":"https://github.com","apiRoot":"https://api.github.com","token":"input your access token"}]';
+    let configs: any[] = JSON.parse(localStorage.getItem('origins') || defaultConfigurations);
+    fillInConfig(getTextArea(), configs);
+
+    document.querySelector('.save-button').addEventListener('click', function(e) {
+        let configurations = getConfigurations(getTextArea());
+        localStorage.setItem('origins', JSON.stringify(configurations));
+        alert('saved')
+    });
+
+    document.querySelector('.test-button').addEventListener('click', (button) => {
+        let configurations = getConfigurations(getTextArea());
+        Promise.all(configurations.map((c: Config) => testToken(c)))
+            .then((promises) => {
+                alert('all ok');
+            })
+            .catch((e) => {
+                alert('ng: ' + e);
+            });
+    });
+});
